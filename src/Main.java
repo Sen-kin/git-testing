732b3d1de8edf7b4ce8812519a71e4f8cc9dead6
Arseniy <arseniyg2806@gmail.com>
2025-03-06 14:09:33 +0300

    Changing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Hi!");
         System.out.println("I need to commit something.");
-    // need to fetch something for testing... 
+    // need to fetch something for testing...
     }
 }
